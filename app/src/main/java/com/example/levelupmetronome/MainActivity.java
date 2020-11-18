40c79d4c1ef9eabc064a039fec4c3b830594ef0a
chrissyast <cjastles@gmail.com>
2020-11-18 18:22:03 +0000

    create right amount of silence and loop the beat

--- app/src/main/java/com/example/levelupmetronome/MainActivity.java
+++ app/src/main/java/com/example/levelupmetronome/MainActivity.java
@@ -5,79 +5,99 @@ import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioTrack;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
-import java.util.Timer;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private Timer myTimer;
     private AudioTrack player;
 
     public void playSound()  {
         player.play();
-        player.pause();
-        player.reloadStaticData();
-        // player.play();
+    }
+
+    public void initialiseSound(int bpm) {
+        InputStream firstBeat = getResources().openRawResource(R.raw.beat1);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        byte[] b = new byte[1024];
+        try {
+            for (int readNum; (readNum = firstBeat.read(b)) != -1; ) {
+                bos.write(b, 0, readNum);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        int startOfData = bos.toString().indexOf("data")+8;
+        byte[] byteArray = bos.toByteArray();
+        int millis = 1000 * 60 / bpm;
+
+        try {
+            WaveFileReader reader = new WaveFileReader(byteArray);
+            byte[] fullLengthByteArray = addSilence(bos, millis, reader);
+            int fullBufferSize = fullLengthByteArray.length - startOfData;
+            int bytesPerSample = reader.getBitsPerSample() * reader.getNumChannels() / 8;
+
+        player = new AudioTrack.Builder()
+                .setTransferMode(AudioTrack.MODE_STATIC)
+                .setAudioAttributes(new AudioAttributes.Builder()
+                        //        .setUsage(AudioAttributes.USAGE_ALARM)              //   \ not causing problems
+                        //        .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC) //   / but don't seem necessary
+                        //        .setLegacyStreamType(AudioManager.STREAM_MUSIC)  // causes blip on first rep
+                        .build())
+                .setAudioFormat(new AudioFormat.Builder()
+                                 .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
+                                 .setSampleRate(44100)
+                                 .setChannelMask(AudioFormat.CHANNEL_OUT_STEREO)
+                        .build())
+                .setBufferSizeInBytes(fullBufferSize)
+                .setSessionId(AudioManager.AUDIO_SESSION_ID_GENERATE)
+                .build();
+
+        player.write(fullLengthByteArray,startOfData,fullLengthByteArray.length - startOfData);
+        player.setLoopPoints(0,fullBufferSize/bytesPerSample,-1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void handleButtonPress(View view) {
-        //MediaPlayer beat = MediaPlayer.create(this, R.raw.beat1);
-        final Handler handler = new Handler(Looper.getMainLooper());
-        final Runnable loopSound = new Runnable() {
-            @Override
-            public void run() {
-                playSound();
-                handler.postDelayed(this, 300);
-            }
-        };
-        handler.post(loopSound);
-
+        playSound();
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        InputStream foo = getResources().openRawResource(R.raw.beat116);
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        byte[] b = new byte[1024];
-        try {
-            for (int readNum; (readNum = foo.read(b)) != -1; ) {
-                bos.write(b, 0, readNum);
-            }
-        } catch (Exception e) {
-            System.out.println(e.getStackTrace().toString());
-        }
-        int startOfData = bos.toString().indexOf("data")+8;
-
-        int minBuffSize = AudioTrack.getMinBufferSize(48000
-                , AudioFormat.CHANNEL_CONFIGURATION_MONO, AudioFormat.ENCODING_PCM_16BIT);
-
-
-        player = new AudioTrack.Builder()
-                .setAudioAttributes(new AudioAttributes.Builder()
-                        .setUsage(AudioAttributes.USAGE_ALARM)
-                        .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
-                        .setLegacyStreamType(AudioManager.STREAM_MUSIC)
-                        .build())
-                .setAudioFormat(new AudioFormat.Builder()
-                        .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
-                        .setSampleRate(44100)
-                        .setChannelMask(AudioFormat.CHANNEL_OUT_STEREO)
-                        .build())
-                .setBufferSizeInBytes(minBuffSize)
-                .setTransferMode(AudioTrack.MODE_STATIC)
-                .build();
-
-        player.write(bos.toByteArray(),startOfData,bos.toByteArray().length - startOfData);
+        initialiseSound(300);
 
         setContentView(R.layout.activity_main);
     }
+
+    private byte[] addSilence(ByteArrayOutputStream originalSound, int intendedMillis, WaveFileReader reader) {
+        int currentDataSize = reader.getDataSize();
+        double bytesPerMilli = reader.dataSizePerMillisecond();
+        int sampleBytes = reader.getBitsPerSample() * reader.getNumChannels() / 8;
+        int intendedDataSize = (int) Math.round((intendedMillis * bytesPerMilli) / sampleBytes) * sampleBytes ;
+        int shortFall = intendedDataSize - currentDataSize;
+        byte[] origSoundBA = originalSound.toByteArray();
+        int origFileSize = origSoundBA.length;
+        byte[] output = new byte[origFileSize + shortFall];
+        int index = 0;
+        for (byte b : origSoundBA) {
+            if (index < output.length) {
+             output[index] = b;
+             index++;
+            }
+        }
+        while (index < output.length) {
+            output[index] = 0;
+            index++;
+        }
+        return output;
+    }
+
 }
---
+++ app/src/main/java/com/example/levelupmetronome/WaveFileReader.java
@@ -0,0 +1,134 @@
+package com.example.levelupmetronome;
+
+import java.io.ByteArrayOutputStream;
+import java.lang.reflect.Field;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class WaveFileReader {
+
+    private final WavFileProperty CHUNK_SIZE = new WavFileProperty(4,4,WavFilePropertyFormat.HEX_LITTLE_ENDIAN, null, "chunkSize");
+    private final WavFileProperty NUM_CHANNELS = new WavFileProperty(10,2,WavFilePropertyFormat.HEX_LITTLE_ENDIAN, "fmt ", "numChannels");
+    private final WavFileProperty SAMPLE_RATE = new WavFileProperty(12,4,WavFilePropertyFormat.HEX_LITTLE_ENDIAN, "fmt ", "sampleRate");
+    private final WavFileProperty BITS_PER_SAMPLE = new WavFileProperty(22,2,WavFilePropertyFormat.HEX_LITTLE_ENDIAN, "fmt ", "bitsPerSample");
+    private final WavFileProperty DATA_SIZE = new WavFileProperty(4,4,WavFilePropertyFormat.HEX_LITTLE_ENDIAN, "data", "dataSize");
+
+    private final List<WavFileProperty> properties = Arrays.asList(CHUNK_SIZE, NUM_CHANNELS, SAMPLE_RATE, DATA_SIZE, BITS_PER_SAMPLE);
+
+
+    private int chunkSize;
+    private int numChannels;
+    private int sampleRate;
+    private int dataSize;
+    private int bitsPerSample;
+
+    public WaveFileReader (byte[] data) throws NoSuchFieldException, IllegalAccessException {
+        {
+            for (WavFileProperty property : properties) {
+                int chunkStart = 0;
+                if (null != property.chunk) {
+                    chunkStart = startOfChunk(data, property.chunk);
+                }
+                property.index = chunkStart + property.offsetWithinChunk;
+                int start = property.index;
+                int length = property.length;
+                byte[] propertyData = Arrays.copyOfRange(data, start, start+length );
+                int foo = (int) makePropertyLegible(property, propertyData);
+                Field field = getClass().getDeclaredField(property.propertyName);
+                field.setInt(this, foo);
+            }
+        }
+    }
+
+    public static Object makePropertyLegible(WavFileProperty property, byte[] data) {
+        if (property.format == WavFilePropertyFormat.ASCII) {
+            StringBuilder sb = new StringBuilder();
+            for (byte datum : data) {
+                sb.append((char) datum);
+            }
+            return sb.toString();
+        }
+        if (property.format == WavFilePropertyFormat.HEX_LITTLE_ENDIAN) {
+            ByteBuffer bb = ByteBuffer.wrap(data);
+            bb.order( ByteOrder.LITTLE_ENDIAN);
+            int output = 0;
+            int multiplier = 1;
+            while( bb.hasRemaining()) {
+
+                short v = bb.getShort();
+                long segment = (v & 0XFFFF) * multiplier;
+                output += segment;
+                multiplier *= (256 * 256);
+            }
+            return output;
+        }
+        return null;
+    }
+
+    private int startOfChunk(byte[] bytes, String chunkName) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream(bytes.length);
+        baos.write(bytes, 0, bytes.length);
+        return baos.toString().indexOf(chunkName);
+    }
+
+    public static class WavFileProperty {
+        private int offsetWithinChunk;
+        private int length;
+        private int index;
+        private WavFilePropertyFormat format;
+        private String chunk;
+        private String propertyName;
+
+
+        public WavFileProperty(Integer offsetWithinChunk, int length, WavFilePropertyFormat format, String chunk, String propertyName) {
+            if (offsetWithinChunk != null) {
+                this.offsetWithinChunk = offsetWithinChunk;
+            }
+            this.length = length;
+            this.format = format;
+            this.chunk = chunk;
+            this.propertyName = propertyName;
+        }
+
+        public void setIndex(int index) {
+            this.index = index;
+        }
+    }
+
+    private enum WavFilePropertyFormat {
+        ASCII,
+        HEX_LITTLE_ENDIAN,
+        HEX_BIG_ENDIAN
+    }
+
+    public int getChunkSize() {
+        return chunkSize;
+    }
+
+    public int getNumChannels() {
+        return numChannels;
+    }
+
+    public int getSampleRate() {
+        return sampleRate;
+    }
+
+    public int getDataSize() {
+        return dataSize;
+    }
+
+    public int getBitsPerSample() {
+        return bitsPerSample;
+    }
+
+    public int fileLengthInMilliseconds() {
+        return (int) ((double) dataSize / (bitsPerSample / 8) / numChannels / sampleRate * 1000);
+    }
+
+    public double dataSizePerMillisecond() {
+        return ((double) sampleRate * numChannels * bitsPerSample  / 8 / 1000);
+    }
+}
